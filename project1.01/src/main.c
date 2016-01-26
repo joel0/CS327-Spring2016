e733b2d90918ce618f60c080a66e457a6d29f50d
Joel May <joel.may@live.com>
2016-01-25 21:04:23 -0600

    Project 1.01 started

---
+++ project1.01/src/main.c
@@ -0,0 +1,13 @@
+//
+// Created by joelm on 2016-01-25.
+//
+
+#include "main.h"
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    printf("Project1.01\n");
+	room_t t;
+
+    return 0;
+}
---
+++ project1.01/src/main.h
@@ -0,0 +1,15 @@
+//
+// Created by joelm on 2016-01-25.
+//
+
+#ifndef PROJECT_MAIN_H
+#define PROJECT_MAIN_H
+
+typedef struct room_struct {
+    int x;
+    int y;
+    int height;
+    int width;
+} room_t;
+
+#endif //PROJECT_MAIN_H
