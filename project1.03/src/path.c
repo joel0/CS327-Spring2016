a7923f9fd852bbcda595356daa9ce7f621f79da0
Joel May <joel.may@live.com>
2016-02-07 23:54:07 -0600

    Imported the provided priority queue and created a file for the path finding.

---
+++ project1.03/src/path.c
@@ -0,0 +1,7 @@
+//
+// Created by joelm on 2016-02-07.
+//
+
+#include "path.h"
+#include "dungeon.h"
+
---
+++ project1.03/src/path.h
@@ -0,0 +1,8 @@
+//
+// Created by joelm on 2016-02-07.
+//
+
+#ifndef PROJECT_PATH_H
+#define PROJECT_PATH_H
+
+#endif //PROJECT_PATH_H
