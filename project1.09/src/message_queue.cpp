cfe5a5f0cf01e047fd400af0db3db61247cbc662
Joel May <joel.may@live.com>
2016-04-12 18:07:04 -0500

    Message queue

---
+++ project1.09/src/message_queue.cpp
@@ -0,0 +1,40 @@
+//
+// Created by joelm on 2016-04-12.
+//
+
+#include <curses.h>
+
+#include "message_queue.h"
+#include "screen.h"
+
+void message_queue::enqueue(std::string* msg) {
+    queue.push_back(*msg);
+}
+
+void message_queue::print_all() {
+    if (queue.size() > 1) {
+        for (int i = 0; i < queue.size(); i++) {
+            screenClearRow(0);
+            mvprintw(0, 0, "(%d/%d) %s", i + 1, queue.size(), queue[i].c_str());
+            getch();
+        }
+    } else if (queue.size() == 1) {
+        screenClearRow(0);
+        mvprintw(0, 0, "%s", queue[0].c_str());
+    }
+    queue.clear();
+}
+
+message_queue* message_queue::queue_instance = NULL;
+
+message_queue *message_queue::instance() {
+    if (queue_instance == NULL) {
+        queue_instance = new message_queue();
+    }
+    return queue_instance;
+}
+
+
+
+
+
---
+++ project1.09/src/message_queue.h
@@ -0,0 +1,22 @@
+//
+// Created by joelm on 2016-04-12.
+//
+
+#ifndef PROJECT0_MESSAGE_QUEUE_H
+#define PROJECT0_MESSAGE_QUEUE_H
+
+#include <string>
+#include <vector>
+
+class message_queue {
+private:
+    static message_queue* queue_instance;
+    std::vector<std::string> queue;
+
+public:
+    static message_queue* instance();
+    void enqueue(std::string* msg);
+    void print_all();
+};
+
+#endif //PROJECT0_MESSAGE_QUEUE_H
